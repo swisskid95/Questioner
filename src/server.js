aab57c36e84c70015f971441a3815036eef4526c
Sanusi Babatunde <swisskid95@gmail.com>
2018-12-31 09:09:17 +0100

    Setup babel and add server.js
    in order to use the latest es6 syntax
    and to support old browsers babel is
    being set up, as it would transpile to later
    version of javascript all the code writting in es6.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+import express from 'express';
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+  console.log(`Starting server on port ${port}...`);
 });
