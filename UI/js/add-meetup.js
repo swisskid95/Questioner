56d0dae1f7ab0e5084e209ec08541d654a10934f
Sanusi Babatunde <swisskid95@gmail.com>
2019-01-09 00:45:00 +0100

    Implement LFA Feedback on UI

--- UI/js/add-meetup.js
+++
@@ -1,9 +0,0 @@
-const addMeetupBtn = document.querySelector('.add-meetup-btn');
-const meetupForm = document.querySelector('.add-meetup-form');
-
-const displayMeetupForm = () => {
-  // console.log(meetupForm.style.display);
-  meetupForm.style.display = meetupForm.style.display === "none" ? "block" : "none";
-}
-
-addMeetupBtn.addEventListener('click', displayMeetupForm);
