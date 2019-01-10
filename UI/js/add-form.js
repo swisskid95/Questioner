98e93ec5f3898bba95ecdd39185a018808d8ba6e
Sanusi Babatunde <swisskid95@gmail.com>
2019-01-10 14:30:43 +0100

    complete UI feedback

--- UI/js/add-form.js
+++ UI/js/add-form.js
@@ -2,7 +2,6 @@ const addBtn = document.querySelector('.add-btn');
 const hiddenForm = document.querySelector('.add-form');
 
 const displayForm = () => {
-  // console.log(meetupForm.style.display);
   hiddenForm.style.display = hiddenForm.style.display === "none" ? "block" : "none";
 }
 
