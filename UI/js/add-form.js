2aabc77b158c163414a2fd06bc7a40d8bf1aa9c5
Sanusi Babatunde <swisskid95@gmail.com>
2019-01-10 22:47:10 +0100

    resolve eslint error in ui js

---
+++ UI/js/add-form.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+const addBtn = document.querySelector('.add-btn');
+const hiddenForm = document.querySelector('.add-form');
+
+const displayForm = () => {
+  hiddenForm.style.display = hiddenForm.style.display === 'none' ? 'block' : 'none';
+};
+
+addBtn.addEventListener('click', displayForm);
